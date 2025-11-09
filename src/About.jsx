d78a7c28b5af58cff09c75392134530984384d47
Yash Arya <yasharya2901@gmail.com>
2025-11-09 23:17:18 +0530

    My Second Commit

--- src/About.jsx
+++ src/About.jsx
@@ -7,7 +7,7 @@ function About() {
       <h1>About Me</h1>
       <p>I am Yash Arya. My roll no. is 2023ebcs756. This is web page.</p>
       <br />
-      <button onClick={() => navigate('/')}>Go to Home</button>
+      <button onClick={() => navigate('/')}>Home Page</button>
     </>
   )
 }
